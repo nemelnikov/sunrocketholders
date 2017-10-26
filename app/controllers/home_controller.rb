7fc4b5e2759cddf258027ca9dd0f54dcf0e29b45
Aldar Antonov <aantonov84@gmail.com>
2017-10-26 15:25:21 +0300

    add different behaviour on first, second and other visits

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,17 @@
 class HomeController < ApplicationController
   def index
     @amount=Link.last.id
-    @number=rand(1 ..@amount)
+    if !session[:visit]
+      @number=rand(1 ..@amount)
+      session[:visit] = 1
+    elsif session[:visit] == 1
+      @number=rand(1 ..@amount)
+      session[:visit] = 2
+      session[:number] = @number
+    elsif session[:visit] == 2
+      @number = session[:number]
+    end
+
     @link=Link.find(@number)
   end
 end
